2de2d731652c377cb33b52a1d188b7c14bede5f5
Vishal Damodar <thatvishal007@gmail.com>
2025-12-08 14:57:10 +0530

    Update Babel and Webpack configs for module handling

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = {
         ['@babel/preset-env', {
             targets: {
                 browsers: ['last 2 versions', 'ie >= 11']
-            }
+            },
+            modules: false
         }]
     ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             }
