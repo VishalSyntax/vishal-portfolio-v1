7794d7604fee723f45acc003a29a11c9f4201bc9
Vishal Damodar <thatvishal007@gmail.com>
2025-12-11 23:51:47 +0530

    Refactor Home
    Updated the Home component to retrieve personal information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { usePortfolio } from '../context/PortfolioContext';
 
-const Home = ({ personalInfo }) => {
+const Home = () => {
+    const { state } = usePortfolio();
+    
     return (
         <section id="home" className="hero-section">
             <div className="container">
-                <h1>{personalInfo.name}</h1>
-                <p className="lead">{personalInfo.title}</p>
+                <h1>{state.profile.name}</h1>
+                <p className="lead">{state.profile.title}</p>
                 <p>Building modern web applications with passion</p>
             </div>
         </section>
